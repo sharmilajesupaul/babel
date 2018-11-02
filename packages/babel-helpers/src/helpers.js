3ad4dfe1ae43486ba838eba33dee44121009cf3e
Sharmila <sharmila.jesupaul@airbnb.com>
2018-11-01 18:38:06 -0700

    Fixes #7597
    Update babel helpers to not require Symbol to exist. We ran into an issue with ie11 where Symbol is not supported. This PR gaurds the usage of Symbol by ensuring that it exists first. There are also changes that allow objects with a length property and array-like objects to work with `iterableToArray`.

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -861,8 +861,13 @@ helpers.arrayWithHoles = helper("7.0.0-beta.0")`
 helpers.iterableToArray = helper("7.0.0-beta.0")`
   export default function _iterableToArray(iter) {
     if (
-      Symbol.iterator in Object(iter) ||
-      Object.prototype.toString.call(iter) === "[object Arguments]"
+      Array.isArray(iter)
+      || typeof iter === 'string'
+      || (typeof Symbol === 'function' && Symbol.iterator in Object(iter))
+      || (iter && 'length' in iter)
+      || (typeof Map !== 'undefined' && iter instanceof Map)
+      || (typeof Set !== 'undefined' && iter instanceof Set)
+      || Object.prototype.toString.call(iter) === "[object Arguments]"
     ) return Array.from(iter);
   }
 `;
@@ -1539,7 +1544,9 @@ helpers.decorate = helper("7.0.2")`
       value: "Descriptor",
       configurable: true,
     };
-    Object.defineProperty(obj, Symbol.toStringTag, desc);
+
+    if (typeof Symbol === 'function' && Symbol.toStringTag)
+      Object.defineProperty(obj, Symbol.toStringTag, desc);
 
     if (element.kind === "field") obj.initializer = element.initializer;
 
@@ -1653,7 +1660,9 @@ helpers.decorate = helper("7.0.2")`
     };
 
     var desc = { value: "Descriptor", configurable: true };
-    Object.defineProperty(obj, Symbol.toStringTag, desc);
+
+    if (typeof Symbol === 'function' && Symbol.toStringTag)
+      Object.defineProperty(obj, Symbol.toStringTag, desc);
 
     return obj;
   }
